630668be9bbb2232cdfde2e0a504a27e7bd309b7
RENARD Thomas <thomrenard@enseirb.fr>
2026-01-12 17:32:30 +0100

    Corge fix

--- src/main/java/com/jad/Foo.java
+++ src/main/java/com/jad/Foo.java
@@ -20,7 +20,15 @@ public class Foo {
     }
 
     public void setCorge(Corge corge) {
+        if (this.corge != null) {
+            this.corge.setFoo(null);
+        }
+
         this.corge = corge;
+
+        if (corge != null && corge.getFoo() != this) {
+            corge.setFoo(this);
+        }
     }
 
     public List<Grault> getGraults() {
